ceae7660c670c593eec1e19c249743073efe249f
EJ Brown <ejbrown3c@gmail.com>
2021-10-15 17:31:37 -0400

    commit tree bcc14e9486c1ae482d1cda37dcbd8d2fb7649f51

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+// Write your solution
+
+var customerName = "bob";
+
+function upperCaseCustomerName (){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer () {
+    bestCustomer = "maybe bob"
+}
+
+
+const leastFavoriteCustomer = 'Tom talks too much.'
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = 'its jim'
+    console.log (leastFavoriteCustomer);
+}
